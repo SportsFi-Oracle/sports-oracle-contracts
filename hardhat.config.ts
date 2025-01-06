db633735d101915b5dc4950f1e03168cfa08fab2
Kevin Kamto <kamtokevin@gmail.com>
2025-01-06 13:17:13 -0500

    feat: completed hardhat config

---
+++ hardhat.config.ts
@@ -0,0 +1,21 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+import { extendConfig, HardhatUserConfig, task } from 'hardhat/config';
+import '@nomiclabs/hardhat-ethers';
+import '@nomicfoundation/hardhat-ignition';
+import 'dotenv/config';
+
+
+const config: HardhatUserConfig = {
+
+  solidity: "0.8.28",
+  networks: {
+    sx_testnet: {
+        url: 'https://rpc.toronto.sx.technology',
+        accounts: [process.env.PRIVATE_KEY!],
+        chainId: 647,
+    }
+  }
+
+}
+
+export default config;
